e00f6a59fde0ee8207153036752df087df489daf
Tien Nguyen <nnti3n@gmail.com>
2020-05-22 20:39:58 +0700

    config

--- lib/application.ex
+++ lib/application.ex
@@ -3,10 +3,14 @@ defmodule Play.Application do
   require Logger
 
   def start(_type, _args) do
-    IO.puts "PORT: #{System.get_env("PORT")}"
+    IO.puts("PORT: #{System.get_env("PORT")}")
+
     children = [
-      {Plug.Cowboy,
-       scheme: :http, plug: Play.Router, options: [port: System.get_env("PORT") || 8080]}
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: Play.Router,
+        options: [port: System.get_env("PORT") || 8080]
+      )
     ]
 
     opts = [strategy: :one_for_one, name: Play.Supervisor]
